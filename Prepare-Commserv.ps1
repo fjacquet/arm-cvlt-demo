0c097dd4589287eb42bf8241f418c591a47c9c4a
Frederic Jacquet <fred@ljf.ch>
2020-11-01 20:53:52 +0100

    Summary of changes in 50 characters or less
    Why:
    Auto setup of some Windows configuration
    
    Issue:
    
    Use 3rd party modules
    
    Reviewer:

--- Prepare-Commserv.ps1
+++ Prepare-Commserv.ps1
@@ -29,10 +29,10 @@ Set-SmbServerConfiguration -ServerHidden $true `
     -Confirm:$false
 
 # Disable-InternetExplorerESC
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+# $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+# $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+# Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+# Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
 
 # Install useful tools
 Set-ExecutionPolicy Bypass -Scope Process -Force  #DevSkim: ignore DS113853
@@ -53,6 +53,10 @@ foreach ($value in $values) {
 Get-Disk | Where-Object {$_.PartitionStyle -eq 'RAW' } | Initialize-Disk
 get-disk -Number 2 | New-Partition -UseMaximumSize  -AssignDriveLetter | Format-Volume -FileSystem ReFS -NewFileSystemLabel "Indexes"  -AllocationUnitSize 64KB -Confirm:$false
 get-disk -Number 3 | New-Partition -UseMaximumSize  -AssignDriveLetter | Format-Volume -FileSystem ReFS -NewFileSystemLabel "DDBs" -AllocationUnitSize 64KB -Confirm:$false
+
+# Go Setup
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File Win10.ps1 -include Win10.psm1 -preset Default.preset
+
 
 # Windows updates
 Install-Module PSWindowsUpdate
