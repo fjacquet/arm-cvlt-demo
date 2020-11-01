5d08675f68da39fa65329daab33cc153edb87450
Frederic Jacquet <fred@ljf.ch>
2020-11-01 18:48:09 +0100

    Summary of changes in 50 characters or less
    Why:
    
    setup ps1 url
    
    Issue:
    init setup
    Reviewer:

---
+++ Prepare-Commserv.ps1
@@ -0,0 +1,60 @@
+mkdir C:\installers\
+
+# Install windows features
+add-windowsfeature -Name RSAT
+add-windowsfeature -Name FS-Data-Deduplication
+
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+# Disable Netbios
+$nicClass = Get-WmiObject -list Win32_NetworkAdapterConfiguration
+$nicClass.enableWins($false, $false)
+
+Set-SmbServerConfiguration `
+    -EnableSMB1Protocol $false `
+    -Confirm:$false
+
+Set-SmbServerConfiguration `
+    -RequireSecuritySignature $true `
+    -EnableSecuritySignature $true `
+    -EncryptData $true `
+    -Confirm:$false
+
+Set-SmbServerConfiguration -AutoShareServer $false `
+    -AutoShareWorkstation $false `
+    -Confirm:$false
+
+Set-SmbServerConfiguration -ServerHidden $true `
+    -AnnounceServer $false `
+    -Confirm:$false
+
+# Disable-InternetExplorerESC
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+# Install useful tools
+Set-ExecutionPolicy Bypass -Scope Process -Force  #DevSkim: ignore DS113853
+Write-Output "installing latest nuget"
+Install-PackageProvider -Name NuGet  -Force -Confirm:$false
+Write-Output "allow ps gallery"
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Write-Output "install chocolatey"
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))#DevSkim: ignore DS104456
+choco feature enable -n allowGlobalConfirmation
+$values = ('googlechrome','sysinternals','7zip.install','vscode')
+foreach ($value in $values) {
+  Write-Output "installing $($value)"
+  choco install $value -y
+}
+
+# Disk preparation
+Get-Disk | Where-Object {$_.PartitionStyle -eq 'RAW' } | Initialize-Disk
+get-disk -Number 2 | New-Partition -UseMaximumSize  -AssignDriveLetter | Format-Volume -FileSystem ReFS -NewFileSystemLabel "Indexes"  -AllocationUnitSize 64KB -Confirm:$false
+get-disk -Number 3 | New-Partition -UseMaximumSize  -AssignDriveLetter | Format-Volume -FileSystem ReFS -NewFileSystemLabel "DDBs" -AllocationUnitSize 64KB -Confirm:$false
+
+# Windows updates
+Install-Module PSWindowsUpdate
+Import-Module -Name PSWindowsUpdate
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
